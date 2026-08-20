afc92d2415e60a149c960ab0d77c420c1b3f993e
mnedel-ops <m.nedel@ufn.edu.br>
2026-08-20 15:12:51 -0300

    Ui Inventario

--- jogo/tcc-movement/Interfaces/bag_component.gd
+++ jogo/tcc-movement/Interfaces/bag_component.gd
@@ -2,9 +2,28 @@ class_name BagComponent
 extends Node
 
 @export var mochila: InventoryData
+@export var inventoryui:Control
+@export var inventory_database: ItemDatabase
+@export var item_list : VBoxContainer
 
 func open_bag() -> void:
 	if mochila == null:
 		return
 	print(mochila.items)
-	# Aqui: abre UI real da mochila.
+	inventoryui.visible=true
+	refresh()
+	
+func _on_menu_pause_menu_closed() -> void:
+	inventoryui.visible = false
+
+func refresh() -> void:
+	if not is_instance_valid(item_list):
+		return
+	for child in item_list.get_children():
+		child.queue_free()
+	if mochila == null or inventory_database == null:
+		return
+	for stack in mochila.get_stacked_view(inventory_database):
+		var label := Label.new()
+		label.text = "%s, %d" % [stack.nome, stack.quantidade]
+		item_list.add_child(label)
